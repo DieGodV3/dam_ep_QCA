31321e471c76f8454a73f35e604ce1ff097dabf6
diego quispe <diego.qc2013@gmail.com>
2023-05-22 15:51:11 -0500

    second

--- DAMEP_RonaldAlagon_KevinCaman_DiegoQuispe/app/src/main/java/com/example/damep_ronaldalagon_kevincaman_diegoquispe/MainActivity.java
+++ DAMEP_RonaldAlagon_KevinCaman_DiegoQuispe/app/src/main/java/com/example/damep_ronaldalagon_kevincaman_diegoquispe/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.damep_ronaldalagon_kevincaman_diegoquispe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.btn_enviar);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Se envió sus datos", Toast.LENGTH_SHORT).show());
     }
 }
